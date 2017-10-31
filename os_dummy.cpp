b0d81a09dda12e2dcf1bf1f0b2ff5f797b8fd28d
Duy-Nguyen Ta <duy@tri.global>
2017-10-31 15:04:40 -0400

    - move RWLockDummy and IPDummy to godotvis - set NO_* define flags in godotvis/SCub

--- os_dummy.cpp
+++ os_dummy.cpp
@@ -29,3 +29,18 @@
 /*************************************************************************/
 #include "os_dummy.h"
 
+RWLock *RWLockDummy::create() {
+	return memnew(RWLockDummy);
+};
+
+void RWLockDummy::make_default() {
+  RWLock::create_func = &RWLockDummy::create;
+};
+
+IP *IPDummy::create() {
+	return memnew(IPDummy);
+};
+
+void IPDummy::make_default() {
+  IP::_create = &IPDummy::create;
+};
