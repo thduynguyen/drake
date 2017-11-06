22ab96aeae1d50d6eb92d2173695dca84321e3d1
Duy-Nguyen Ta <duy@tri.global>
2017-11-06 15:26:32 -0500

    simple material with only the albedo color works

--- godotvis.cpp
+++ godotvis.cpp
@@ -21,10 +21,44 @@ void SetupScene() {
 	VisualServer *vs = VisualServer::get_singleton();
 	RID scenario = vs->scenario_create();
 
+	RID camera = vs->camera_create();
+
+  //Transform Tc_in = Transform(Basis(Vector3(0.0, -90.0 * DEGREE, 0.0)).transposed(), Vector3(20.0, 0.0, 0.0));
+  Transform Tc;
+  Vector3 position;
+  Tc.set_look_at(Vector3(20., 0., 0.), position, Vector3(0, 1, 0));
+  vs->camera_set_transform(camera, Tc);
+	vs->camera_set_perspective(camera, 65, 0.1, 100);
+
+
+	RID viewport = vs->viewport_create();
+	Size2i screen_size = OS::get_singleton()->get_window_size();
+	vs->viewport_set_size(viewport, screen_size.x, screen_size.y);
+	vs->viewport_attach_to_screen(viewport, Rect2(Vector2(), screen_size));
+	vs->viewport_set_active(viewport, true);
+	vs->viewport_attach_camera(viewport, camera);
+	vs->viewport_set_scenario(viewport, scenario);
+
+
+  SpatialMaterial* material = memnew(SpatialMaterial);
+  material->set_albedo(Color(0.0, 1., 0.));
+  material->set_specular(0.8);
+  material->set_metallic(0.2);
+  material->set_roughness(0.5);
+  SpatialMaterial* material2 = memnew(SpatialMaterial);
+  material2->set_albedo(Color(1.0, 0., 0.));
+  // IMPORTANT: This calls to SpatialMaterial::_update_shader(). Without this materials wont' work
+  // Godot sets up so that this is called in SceneTree::call_idle_call_backs()
+  // Not sure if we need to do this as an idle callback...
+  SpatialMaterial::flush_changes();
+
+
   String filename = path + "godot_ball.mesh";
-  // This calls all the way down to RasterizerStorageGLES3::mesh_add_surface(), which initialize VAO
+  // This calls all the way down to RasterizerStorageGLES3::mesh_add_surface(), which initializes VAO
   // RasterizerStorageGLES3::mesh_add_surface <-- ArrayMesh::add_surface <-- ArrayMesh::_setv() <-- res->set() <-- ResourceInteractiveLoaderBinary::poll()
   Ref<Mesh> mesh = ResourceLoader::load(filename);
+  //for (int i = 0; i<4; ++i)
+    //vs->mesh_surface_set_material(mesh->get_rid(), i, material->get_rid());
 
 	Vector<Vector3> vts;
 
@@ -43,14 +77,11 @@ void SetupScene() {
   //RID test_cube = vs->get_test_cube();
   RID test_cube = vs->mesh_create();
   vs->mesh_add_surface_from_mesh_data(test_cube, md);
-
   RID instance = vs->instance_create2(test_cube, scenario);
 
   //RID mesh_id = mesh->get_rid();
-  SpatialMaterial* material = memnew(SpatialMaterial);
-  material->set_albedo(Color(1.0, 0., 0.));
-  vs->mesh_surface_set_material(test_cube, 0, material->get_rid());
-  std::cout << "NUM SURFACES: " << vs->mesh_get_surface_count(test_cube) << std::endl;
+  vs->mesh_surface_set_material(test_cube, 0, material2->get_rid());
+  std::wcout << "NUM SURFACES: " << vs->mesh_get_surface_count(test_cube) << std::endl;
 
   // This calls vs->create_instance() in VisualInstance constructor
   // Using instance_create2() doesn't work, probably because the object_id is not attached to the instance
@@ -59,34 +90,19 @@ void SetupScene() {
   // Doing this is not ideal, however, as nobody manages this pointer, it's not freed at the end
   // hence the program doesn't exit naturally.
   MeshInstance *mesh_instance = memnew(MeshInstance);
-  mesh_instance->set_mesh(mesh);
-  // put this instance into the scenario. This is important as viewport's render_scene
-  // render_camera function checks these instances and culls them out
+  // call this or any function that calls _instance_queue_update
+  // before calling set_surface_material to update VS instance's materials
   vs->instance_set_scenario(mesh_instance->get_instance(), scenario);
-  //RID mesh_instance = vs->instance_create2(mesh->get_rid(), scenario);
-  vs->mesh_surface_set_material(mesh->get_rid(), 0, material->get_rid());
-  Rect3 aabb = mesh->get_aabb();
-  Vector3 position = aabb.position;
+  // Without scenario, set_mesh->set_base->VS::instance_set_base DOES NOT call _instance_queue_update to update
+  // VS instance's materials, causing a mismatch between MeshInstance's and its corresponding VS instance's materials
+  mesh_instance->set_mesh(mesh);
+  // Without instance_queue_update, the following will not work as VS instance's materials has size 0!
+  mesh_instance->set_surface_material(0, material);
+  mesh_instance->set_surface_material(1, material);
+  mesh_instance->set_surface_material(2, material);
+  mesh_instance->set_surface_material(3, material);
 
   //vs->instance_set_transform(instance, Transform(Basis(), Vector3()));
-
-	RID camera = vs->camera_create();
-
-  //Transform Tc_in = Transform(Basis(Vector3(0.0, -90.0 * DEGREE, 0.0)).transposed(), Vector3(20.0, 0.0, 0.0));
-  Transform Tc;
-  Tc.set_look_at(Vector3(20., 0., 0.), position, Vector3(0, 1, 0));
-  vs->camera_set_transform(camera, Tc);
-	vs->camera_set_perspective(camera, 65, 0.1, 100);
-
-
-	RID viewport = vs->viewport_create();
-	Size2i screen_size = OS::get_singleton()->get_window_size();
-	vs->viewport_set_size(viewport, screen_size.x, screen_size.y);
-	vs->viewport_attach_to_screen(viewport, Rect2(Vector2(), screen_size));
-	vs->viewport_set_active(viewport, true);
-	vs->viewport_attach_camera(viewport, camera);
-	vs->viewport_set_scenario(viewport, scenario);
-
 
 	/*
 		RID lightaux = vs->light_create( VisualServer::LIGHT_OMNI );
@@ -103,8 +119,8 @@ void SetupScene() {
 	//vs->light_set_shadow( lightaux, true );
 	RID light = vs->instance_create2(lightaux, scenario);
 	Transform lla;
-	//lla.set_look_at(Vector3(),Vector3(1,-1,1),Vector3(0,1,0));
-	lla.set_look_at(Vector3(20., 0, 0), position, Vector3(0, 1, 0));
+  lla.set_look_at(Vector3(),Vector3(1,-1,1),Vector3(0,1,0));
+	//lla.set_look_at(Vector3(20., 0, 0), position, Vector3(0, 1, 0));
 
 	vs->instance_set_transform(light, lla);
 
