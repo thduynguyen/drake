9f6e028e388dba8c6ae063b4c8745fb2dd7b69d7
Duy-Nguyen Ta <thduynguyen@gmail.com>
2017-12-15 15:15:23 -0500

    rgbd_renderer_test.h -> rgbd_renderer_test_util.h

--- systems/sensors/test/rgbd_renderer_test_util.h
+++ systems/sensors/test/rgbd_renderer_test_util.h
@@ -14,6 +14,20 @@ namespace systems {
 namespace sensors {
 namespace test {
 
+using Eigen::Isometry3d;
+
+const int kWidth = 640;
+const int kHeight = 480;
+const double kZNear = 0.5;
+const double kZFar = 5.;
+const double kFovY = M_PI_4;
+const bool kShowWindow = true;
+
+// The following tolerance is used due to a precision difference between Ubuntu
+// Linux and Mac OSX.
+const double kColorPixelTolerance = 1.001;
+const double kDepthTolerance = 1e-4;
+
 // Holds `(x, y)` indices of the screen coordinate system where the ranges of
 // `x` and `y` are [0, kWidth) and [0, kHeight) respectively.
 struct ScreenCoord {
@@ -23,24 +37,21 @@ struct ScreenCoord {
 
 void CompareColor(const uint8_t* pixel,
                   const sensors::ColorI& color,
-                  int alpha, double tolerance) {
-  // Use ASSERT here instead of EXPECT_NEAR to stop all subsequent testing,
-  // because this function could be called inside for loops to search over all
-  // the pixels in an image.
-  ASSERT_NEAR(pixel[0], color.r, tolerance);
-  ASSERT_NEAR(pixel[1], color.g, tolerance);
-  ASSERT_NEAR(pixel[2], color.b, tolerance);
+                  int alpha) {
+  ASSERT_NEAR(pixel[0], color.r, kColorPixelTolerance);
+  ASSERT_NEAR(pixel[1], color.g, kColorPixelTolerance);
+  ASSERT_NEAR(pixel[2], color.b, kColorPixelTolerance);
   ASSERT_EQ(pixel[3], alpha);
 }
 
 // This suite tests RgbdRenderer.
-template <class Renderer>
+template <class RGBD_RENDERER>
 class RgbdRendererTest : public ::testing::Test {
  public:
   RgbdRendererTest() :
       color_(kWidth, kHeight), depth_(kWidth, kHeight), label_(kWidth, kHeight),
       // Looking strait down from 3m above the ground.
-      X_WC_(Eigen::Translation3d(0, 0, kDefaultDistance) *
+      X_WR_(Eigen::Translation3d(0, 0, kDefaultDistance) *
             Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitY()) *
             Eigen::AngleAxisd(-M_PI_2, Eigen::Vector3d::UnitZ())) {}
 
@@ -54,7 +65,7 @@ class RgbdRendererTest : public ::testing::Test {
   void VerifyUniformColor(const sensors::ColorI& pixel, int alpha) {
     for (int y = 0; y < kHeight; ++y) {
       for (int x = 0; x < kWidth; ++x) {
-        CompareColor(color_.at(x, y), pixel, alpha, kColorPixelTolerance);
+        CompareColor(color_.at(x, y), pixel, alpha);
       }
     }
   }
@@ -77,12 +88,12 @@ class RgbdRendererTest : public ::testing::Test {
 
   // Verifies the chosen pixels, i.e. `kOutliers`, belong to the ground.
   void VerifyOutliers() {
-    const auto& kTerrain = renderer_->color_palette().get_terrain_color();
+    const auto& kTerrain = renderer_->get_flat_terrain_color();
     for (const auto& screen_coord : kOutliers) {
       const int x = screen_coord.x;
       const int y = screen_coord.y;
       // Color
-      CompareColor(color_.at(x, y), kTerrain, 255u, kColorPixelTolerance);
+      CompareColor(color_.at(x, y), kTerrain, 255u);
       // Depth
       ASSERT_NEAR(depth_.at(x, y)[0], kDefaultDistance, kDepthTolerance);
       // Label
@@ -90,26 +101,16 @@ class RgbdRendererTest : public ::testing::Test {
     }
   }
 
+  void SetUp() override {}
+
   // All tests on this class must invoke this first.
-  void Init(const Eigen::Isometry3d& X_WC, bool add_terrain = false) {
-    renderer_ = std::make_unique<Renderer>(
-        RenderingConfig{kWidth, kHeight, kFovY, kZNear, kZFar, kShowWindow},
-        X_WC);
+  void SetUp(const Eigen::Isometry3d& X_WR, bool add_terrain = false) {
+    renderer_ = std::make_unique<RGBD_RENDERER>(
+        X_WR, kWidth, kHeight, kZNear, kZFar, kFovY, kShowWindow);
 
-    if (add_terrain) renderer_->AddFlatTerrain();
+    if (add_terrain)
+      renderer_->AddFlatTerrain();
   }
-
-  const int kWidth = 640;
-  const int kHeight = 480;
-  const double kZNear = 0.5;
-  const double kZFar = 5.;
-  const double kFovY = M_PI_4;
-  const bool kShowWindow = false;
-
-  // The following tolerance is used due to a precision difference between
-  // Ubuntu Linux and Mac OSX.
-  const double kColorPixelTolerance = 1.001;
-  const double kDepthTolerance = 1.1e-4;
 
   const sensors::ColorI kDefaultVisualColor = {179u, 179u, 179u};
   const float kDefaultDistance{3.f};
@@ -124,7 +125,7 @@ class RgbdRendererTest : public ::testing::Test {
   ImageRgba8U color_;
   ImageDepth32F depth_;
   ImageLabel16I label_;
-  Eigen::Isometry3d X_WC_;
+  Isometry3d X_WR_;
 
   std::unique_ptr<RgbdRenderer> renderer_;
 };
